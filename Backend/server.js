533762e966d7f8c7b44c5dcbac0aa438e5d5d932
unknown <mohitparashar0603@gmail.com>
2024-11-14 00:26:14 +0530

    2nd commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+require(`dotenv`).config();
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
 const carRoutes = require('./routes/cars');
 const cors = require('cors');
+const connectDB = require(`./connect`);
 
 const app = express();
 app.use(express.json());
@@ -12,10 +14,23 @@ const corsOptions = {
   };
   app.use(cors(corsOptions)); 
 
-mongoose.connect('mongodb://localhost:27017');
+// mongoose.connect('mongodb://localhost:27017');
 
 app.use('/api/users', userRoutes);
 app.use('/api/cars', carRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    
+      await connectDB(process.env.MONGO_URI);
+      app.listen(PORT, console.log(`Server is listening on PORT ${PORT}`));
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+start();
 
